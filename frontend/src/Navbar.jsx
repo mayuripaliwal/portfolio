adb1d5143e1bc468e297faeb3fc34517be588d3a
Mayuri <mayuripaliwal8@gmail.com>
2026-09-15 17:06:24 +0530

    fix: add href for mobile view navbar

--- frontend/src/Navbar.jsx
+++ frontend/src/Navbar.jsx
@@ -74,28 +74,28 @@ export default function Navbar() {
           {/* Current: "bg-amber-50 border-amber-600 text-amber-700 dark:border-amber-500 dark:bg-amber-600/10 dark:text-amber-400", Default: "border-transparent text-gray-500 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800 dark:hover:border-white/20 dark:hover:bg-white/5 dark:hover:text-white" */}
           <DisclosureButton
             as="a"
-            href="#"
+            href="#about"
             className="block border-l-4 border-amber-600 bg-amber-50 py-2 pr-4 pl-3 text-base font-medium text-amber-700 sm:pr-6 sm:pl-5 dark:border-amber-500 dark:bg-amber-600/10 dark:text-amber-400"
           >
             About
           </DisclosureButton>
           <DisclosureButton
             as="a"
-            href="#"
+            href="#projects"
             className="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800 sm:pr-6 sm:pl-5 dark:text-gray-300 dark:hover:border-white/20 dark:hover:bg-white/5 dark:hover:text-white"
           >
             Projects
           </DisclosureButton>
           <DisclosureButton
             as="a"
-            href="#"
+            href="#experience"
             className="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800 sm:pr-6 sm:pl-5 dark:text-gray-300 dark:hover:border-white/20 dark:hover:bg-white/5 dark:hover:text-white"
           >
             Experience
           </DisclosureButton>
           <DisclosureButton
             as="a"
-            href="#"
+            href="#skills"
             className="block border-l-4 border-transparent py-2 pr-4 pl-3 text-base font-medium text-gray-500 hover:border-gray-300 hover:bg-gray-50 hover:text-gray-800 sm:pr-6 sm:pl-5 dark:text-gray-300 dark:hover:border-white/20 dark:hover:bg-white/5 dark:hover:text-white"
           >
             Skills
